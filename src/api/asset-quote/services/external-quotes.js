833606ee7385cd86a90087a61b37efb598793921
Lee Jun <leejun.nakao@gmail.com>
2022-05-11 00:16:16 +0000

    refactor: rename exchange crypto to CRYPTO

--- src/api/asset-quote/services/external-quotes.js
+++ src/api/asset-quote/services/external-quotes.js
@@ -2,22 +2,25 @@ const { InexistentAsset } = require("../../../utils/aux-entity");
 const { formatSearchResult } = require("../../../utils/parser");
 
 module.exports = {
-    searchExternalQuotes: async (params) => {
-        if (!params.assetCodes.length) return {}
-        const { source } = params;
+  searchExternalQuotes: async (params) => {
+    if (!params.assetCodes.length) return {};
+    const { source } = params;
 
-        const serviceOptions = {
-            B3: strapi.service('api::asset-quote.source-exchange').searchB3,
-            crypto: strapi.service('api::asset-quote.source-exchange').searchCrypto
-        }
+    const serviceOptions = {
+      B3: strapi.service("api::asset-quote.source-exchange").searchB3,
+      CRYPTO: strapi.service("api::asset-quote.source-exchange").searchCrypto,
+    };
 
-        const service = serviceOptions[source];
+    const service = serviceOptions[source];
 
-        return await service(params);
-    },
-    parseExternalQuotes: (quotes) => {
-        const validExternalQuotes = Object.entries(quotes).map(([code, result]) => [code, formatSearchResult(result)]);
+    return await service(params);
+  },
+  parseExternalQuotes: (quotes) => {
+    const validExternalQuotes = Object.entries(quotes).map(([code, result]) => [
+      code,
+      formatSearchResult(result),
+    ]);
 
-        return Object.fromEntries(validExternalQuotes)
-    }
-}
+    return Object.fromEntries(validExternalQuotes);
+  },
+};
